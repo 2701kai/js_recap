a2cd83b6a7ff9d6a1fe14e3439a90dd3cd3586b2
2701kai <2701kai@gmail.com>
2025-03-03 21:39:14 +0100

    cheeses..back to 1st grade, i guess

---
+++ basics/today.js
@@ -0,0 +1,22 @@
+// const teacher = {
+//     name: 'Markus',
+//     kannWas: true
+// };
+
+const student = {
+    name:'kai',
+    kannWas: false,
+    wannabeApprentice: true
+};
+
+function startIfTeacherOk(teacher) {
+    let student;
+    if (teacher === "ok"){
+        student = "startCourse";
+    } else {
+        student = "oder vielleicht doch Laub fegen?";
+    }
+    return student;
+}
+
+console.log(student.name,', ', startIfTeacherOk("not ok")); // "startCourse" oder "oder vielleicht doch Laub fegen?"
